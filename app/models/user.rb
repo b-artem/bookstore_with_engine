5588d7238d002c247005cdfe36cbae50aa2afd92
b-artem <artemb.dev@gmail.com>
2017-12-04 01:25:01 +0200

    fix skip confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
       else
         user.email = auth.info.first_name + auth.info.last_name + '@fb.com'
       end
-      skip_confirmation!
+      user.skip_confirmation!
       user.password = Devise.friendly_token[0, 20]
       user.image_url = auth.info.image
     end
